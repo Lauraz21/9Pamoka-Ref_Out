86536328e71ac8884ae629bca8a3e48058637ad1
Laura <Laura@DESKTOP-BESSB6E>
2023-09-11 21:59:47 +0300

    Add project files.

---
+++ 9Pamoka Ref_Out/Program.cs
@@ -0,0 +1,34 @@
+﻿//1.1
+
+namespace _9Pamoka_Ref_Out
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Swap(ref x, ref y);
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+        }
+        public static void Swap(ref int x, ref int y)
+        {
+            int z = x;
+            x = y;
+            y = z;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
